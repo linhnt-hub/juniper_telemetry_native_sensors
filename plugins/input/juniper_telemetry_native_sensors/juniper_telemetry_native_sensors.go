542a93db1c041e49fb367832262268a8f0736c6e
Nik <59143079+linhnt-hub@users.noreply.github.com>
2025-03-04 10:17:36 +0700

    Update juniper_telemetry_native_sensors.go

--- plugins/input/juniper_telemetry_native_sensors/juniper_telemetry_native_sensors.go
+++ plugins/input/juniper_telemetry_native_sensors/juniper_telemetry_native_sensors.go
@@ -1,92 +1,106 @@
-// Package rand is loosely based off of https://github.com/danielnelson/telegraf-plugins
-package rand
+//go:generate ../../../tools/config_includer/generator
+//go:generate ../../../tools/readme_config_includer/generator
+package juniper_telemetry_native_sensors
 
 import (
-	"context"
-	"math/rand"
+	_ "embed"
+	"net"
+	"sync"
 	"time"
 
 	"github.com/influxdata/telegraf"
-	"github.com/influxdata/telegraf/config"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/plugins/common/socket"
 	"github.com/influxdata/telegraf/plugins/inputs"
 )
 
-type RandomNumberGenerator struct {
-	ValueName       string          `toml:"value_name"`
-	Min             int64           `toml:"min"`
-	Max             int64           `toml:"max"`
-	SampleFrequency config.Duration `toml:"sample_frequency"`
-	ctx             context.Context
-	cancel          context.CancelFunc
+//go:embed sample.conf
+var sampleConfig string
 
-	Log telegraf.Logger `toml:"-"`
+var once sync.Once
+
+type SocketListener struct {
+	ServiceAddress string          `toml:"service_address"`
+	TimeSource     string          `toml:"time_source"`
+	Log            telegraf.Logger `toml:"-"`
+	socket.Config
+	socket.SplitConfig
+
+	socket *socket.Socket
+	parser telegraf.Parser
+}
+
+func (*SocketListener) SampleConfig() string {
+	return sampleConfig
+}
+
+func (sl *SocketListener) SetParser(parser telegraf.Parser) {
+	sl.parser = parser
+}
+
+func (sl *SocketListener) Init() error {
+	sock, err := sl.Config.NewSocket(sl.ServiceAddress, &sl.SplitConfig, sl.Log)
+	if err != nil {
+		return err
+	}
+	sl.socket = sock
+
+	return nil
+}
+
+func (sl *SocketListener) Start(acc telegraf.Accumulator) error {
+	// Create the callbacks for parsing the data and recording issues
+	onData := func(_ net.Addr, data []byte, receiveTime time.Time) {
+		metrics, err := sl.parser.Parse(data)
+
+		if err != nil {
+			acc.AddError(err)
+			return
+		}
+
+		if len(metrics) == 0 {
+			once.Do(func() {
+				sl.Log.Debug(internal.NoMetricsCreatedMsg)
+			})
+		}
+
+		for _, m := range metrics {
+			switch sl.TimeSource {
+			case "", "metric":
+			case "receive_time":
+				m.SetTime(receiveTime)
+			}
+
+			acc.AddMetric(m)
+		}
+	}
+	onError := func(err error) {
+		acc.AddError(err)
+	}
+
+	// Start the listener
+	if err := sl.socket.Setup(); err != nil {
+		return err
+	}
+	sl.socket.Listen(onData, onError)
+	addr := sl.socket.Address()
+	sl.Log.Infof("Listening on %s://%s", addr.Network(), addr.String())
+
+	return nil
+}
+
+func (*SocketListener) Gather(telegraf.Accumulator) error {
+	return nil
+}
+
+func (sl *SocketListener) Stop() {
+	if sl.socket != nil {
+		sl.socket.Close()
+	}
 }
 
 func init() {
-	inputs.Add("rand", func() telegraf.Input {
-		return &RandomNumberGenerator{
-			ValueName:       "value",
-			Min:             0,
-			Max:             100,
-			SampleFrequency: config.Duration(1 * time.Second),
-		}
+	inputs.Add("socket_listener", func() telegraf.Input {
+		return &SocketListener{}
 	})
-}
-
-func (r *RandomNumberGenerator) Init() error {
-	return nil
-}
-
-func (r *RandomNumberGenerator) SampleConfig() string {
-	return `
-  ## Generates random numbers
-	[inputs.rand]
-	# the name of the measurement to write out to.
-	# value_name = "value"
-	# min = 0
-	# max = 100
-	# sample_frequency = "1000ms"
-`
-}
-
-func (r *RandomNumberGenerator) Description() string {
-	return "Generates a random number"
-}
-
-func (r *RandomNumberGenerator) Gather(a telegraf.Accumulator) error {
-	r.sendMetric(a)
-	return nil
-}
-
-// // provide the extra functions so we can also run as a service input.
-// func (r *RandomNumberGenerator) Start(a telegraf.Accumulator) error {
-// 	println("Started as service")
-// 	r.ctx, r.cancel = context.WithCancel(context.Background())
-// 	go func() {
-// 		t := time.NewTicker(r.SampleFrequency)
-// 		for {
-// 			select {
-// 			case <-r.ctx.Done():
-// 				t.Stop()
-// 				return
-// 			case <-t.C:
-// 				r.sendMetric(a)
-// 			}
-// 		}
-// 	}()
-// 	return nil
-// }
-
-func (r *RandomNumberGenerator) Stop() {
-	r.cancel()
-}
-
-func (r *RandomNumberGenerator) sendMetric(a telegraf.Accumulator) {
-	n := rand.Int63n(r.Max-r.Min) + r.Min
-	a.AddFields("random",
-		map[string]interface{}{
-			r.ValueName: n,
-		},
-		nil,
-	)
 }
